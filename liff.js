57f757b9f250071e7c05a955e25c110915e1e7db
39labo <sakuma@39labo.com>
2023-04-18 15:22:32 +0900

    test

--- liff.js
+++ liff.js
@@ -3,6 +3,25 @@ $(document).ready(function () {
     initializeLiff(liffId);
 })
 
+window.onload = function() {
+    var iframe = document.querySelector('iframe');
+    iframe.onload = function() {
+        alert( '読み込み完了' );
+        var elem = document.querySelector('.nYdzXd');
+        console.log(elem);
+        if (elem) {
+            elem.style.visibility = 'hidden';
+        } else {
+            console.log('cant find elem');
+        }
+        var field = document.querySelectorAll('div[role=listitem]:nth-child(1),div[role=listitem]:nth-child(2)');
+        for (var f in field) {
+            field[f].style.visibility = 'hidden';
+            field[f].style.height = 0;
+        }
+    }
+}
+
 function initializeLiff(liffId) {
     liff
         .init({
@@ -17,26 +36,11 @@ function initializeLiff(liffId) {
                 liff.getProfile()
                     .then((profile) => {
                         var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSengIKFHfwaRHsqHLe7E776kG8b7jLU6_COZ_7uCUrQIuEd7Q/viewform?embedded=true&usp=pp_url&entry.1916663609='+profile.userId+'&entry.2046105793='+profile.displayName;
-                        var wrapper = document.getElementById('tf-wrapper');
-                        var iframe = document.createElement('iframe');
+                        var iframe = document.querySelector('iframe');
+                        iframe.setAttribute('src', formUrl);
                         iframe.contentWindow.onload = function() {
                             console.log('load');
-                            var elem = document.querySelector('.nYdzXd');
-                            console.log(elem);
-                            if (elem) {
-                                elem.style.visibility = 'hidden';
-                            } else {
-                                console.log('cant find elem');
-                            }
-                            var field = document.querySelectorAll('div[role=listitem]:nth-child(1),div[role=listitem]:nth-child(2)');
-                            for (var f in field) {
-                                field[f].style.visibility = 'hidden';
-                                field[f].style.height = 0;
-                            }
                         };
-                        iframe.setAttribute('src', formUrl);
-                        wrapper.appendChild(iframe);
-                        
                     }).catch((err) => {
                         window.alert('Error display form: ' + err);
                     });
