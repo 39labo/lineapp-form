15432ba30b82d8f2eaa89990d82145a810184fdd
39labo <sakuma@39labo.com>
2023-04-18 15:40:43 +0900

    finalize

--- liff.js
+++ liff.js
@@ -9,21 +9,14 @@ function initializeLiff(liffId) {
             liffId: liffId
         })
         .then(() => {
-            // Webブラウザからアクセスされた場合は、LINEにログインする
             if (!liff.isInClient() && !liff.isLoggedIn()) {
                 window.alert("LINEアカウントにログインしてください。");
                 liff.login({redirectUri: location.href});
             }else{
-                const idToken = liff.getIDToken();
-                console.log(idToken);
-                liff.getProfile()
-                    .then((profile) => {
-                        var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSengIKFHfwaRHsqHLe7E776kG8b7jLU6_COZ_7uCUrQIuEd7Q/viewform?embedded=true&usp=pp_url&entry.1916663609='+profile.userId+'&entry.2046105793='+profile.displayName;
-                        var iframe = document.querySelector('iframe');
-                        iframe.setAttribute('src', formUrl);
-                    }).catch((err) => {
-                        window.alert('Error display form: ' + err);
-                    });
+                const accessToken = liff.getAccessToken();
+                var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSengIKFHfwaRHsqHLe7E776kG8b7jLU6_COZ_7uCUrQIuEd7Q/viewform?embedded=true&usp=pp_url&entry.1916663609='+accessToken;
+                var iframe = document.querySelector('iframe');
+                iframe.setAttribute('src', formUrl);
             }
         })
         .catch((err) => {
