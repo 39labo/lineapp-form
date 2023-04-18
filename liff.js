efd3641cdd6806cc83bbba5df7f14887594c04e8
39labo <sakuma@39labo.com>
2023-04-18 14:14:46 +0900

    test

--- liff.js
+++ liff.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     initializeLiff(liffId);
 })
 
-$('iframe').on('load', function () {
+function iframeLoaded() {
     console.log('load');
     var elem = iframe.querySelector('.nYdzXd');
     elem.style.visibility = 'hidden';
-})
+}
 
 function initializeLiff(liffId) {
     liff
@@ -20,14 +20,13 @@ function initializeLiff(liffId) {
                 window.alert("LINEアカウントにログインしてください。");
                 liff.login({redirectUri: location.href});
             }else{
-                const idToken = liff.getIDToken();
-                console.log(idToken);
                 liff.getProfile()
                     .then((profile) => {
                         var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSengIKFHfwaRHsqHLe7E776kG8b7jLU6_COZ_7uCUrQIuEd7Q/viewform?embedded=true&usp=pp_url&entry.1916663609='+profile.userId+'&entry.2046105793='+profile.displayName;
                         var wrapper = document.getElementById('tf-wrapper');
                         var iframe = document.createElement('iframe');
                         iframe.setAttribute('src', formUrl);
+                        iframe.onload = iframeLoaded();
                         wrapper.appendChild(iframe);
                     }).catch((err) => {
                         window.alert('Error display form: ' + err);
