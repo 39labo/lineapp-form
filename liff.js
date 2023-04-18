a0ffaff6b79c4dc4d1fe04630d959c53cdd11c0d
39labo <sakuma@39labo.com>
2023-04-18 13:44:57 +0900

    mod

--- liff.js
+++ liff.js
@@ -16,16 +16,20 @@ function initializeLiff(liffId) {
             }else{
                 const idToken = liff.getIDToken();
                 console.log(idToken);
-                const profile = liff.getProfile();
-                console.log(profile);
-                // var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSengIKFHfwaRHsqHLe7E776kG8b7jLU6_COZ_7uCUrQIuEd7Q/viewform?embedded=true&usp=pp_url&entry.2046105793='+idToken;             
-                // var wrapper = document.getElementById('tf-wrapper');
-                // var iframe = document.createElement('iframe');
-                // iframe.setAttribute('src', formUrl)
-                // wrapper.appendChild(iframe);                
+                liff.getProfile()
+                    .then((profile) => {
+                        console.log(profile);
+                        var formUrl = 'https://forms.gle/UUnapwTmTfNPSwjC6?embedded=true&usp=pp_url&c'+profile.userId+'&entry.2046105793='+profile.displayName;
+                        var wrapper = document.getElementById('tf-wrapper');
+                        var iframe = document.createElement('iframe');
+                        iframe.setAttribute('src', formUrl);
+                        wrapper.appendChild(iframe);
+                    }).catch((err) => {
+                        window.alert('Error getting profile: ' + err);
+                    });
             }
         })
         .catch((err) => {
-            window.alert('LIFF Initialization failed ' + err);
+            window.alert('LIFF Initialization failed: ' + err);
         });
 }
