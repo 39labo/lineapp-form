27644001ce391922ac55820e0fe7c0371798e66a
39labo <sakuma@39labo.com>
2023-04-18 15:36:22 +0900

    akirameta

--- liff.js
+++ liff.js
@@ -3,25 +3,6 @@ $(document).ready(function () {
     initializeLiff(liffId);
 })
 
-window.onload = function() {
-    var iframe = document.querySelector('iframe');
-    iframe.onload = function() {
-        alert( '読み込み完了' );
-        var elem = this.contentWindow.document.querySelector('.nYdzXd');
-        console.log(elem);
-        if (elem) {
-            elem.style.visibility = 'hidden';
-        } else {
-            console.log('cant find elem');
-        }
-        var field = this.contentWindow.document.querySelectorAll('div[role=listitem]:nth-child(1),div[role=listitem]:nth-child(2)');
-        for (var f in field) {
-            field[f].style.visibility = 'hidden';
-            field[f].style.height = 0;
-        }
-    }
-}
-
 function initializeLiff(liffId) {
     liff
         .init({
@@ -33,14 +14,13 @@ function initializeLiff(liffId) {
                 window.alert("LINEアカウントにログインしてください。");
                 liff.login({redirectUri: location.href});
             }else{
+                const idToken = liff.getIDToken();
+                console.log(idToken);
                 liff.getProfile()
                     .then((profile) => {
                         var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSengIKFHfwaRHsqHLe7E776kG8b7jLU6_COZ_7uCUrQIuEd7Q/viewform?embedded=true&usp=pp_url&entry.1916663609='+profile.userId+'&entry.2046105793='+profile.displayName;
                         var iframe = document.querySelector('iframe');
                         iframe.setAttribute('src', formUrl);
-                        iframe.contentWindow.onload = function() {
-                            console.log('load');
-                        };
                     }).catch((err) => {
                         window.alert('Error display form: ' + err);
                     });
